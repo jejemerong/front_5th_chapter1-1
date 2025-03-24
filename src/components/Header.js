d8bcdd6be0148ea94032206502e114eece3214f9
jejemerong <9708jjw@gmail.com>
2025-03-24 11:06:42 +0900

    feat: 컴포넌트 분리

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+export const Header = (isLoggedIn) => {
+  return /* HTML */ `
+    <div>
+      <header class="bg-blue-600 text-white p-4 sticky top-0">
+        <h1 class="text-2xl font-bold">항해플러스</h1>
+      </header>
+      <nav class="bg-white shadow-md p-2 sticky top-14">
+        <ul class="flex justify-around">
+          <li><a href="/" class="text-blue-600">홈</a></li>
+          ${isLoggedIn
+            ? `<li><a href="/profile" class="text-gray-600">프로필</a></li>
+          <li><a href="#" class="text-gray-600">로그아웃</a></li>`
+            : `<li><a href="/login" class="text-gray-600">로그인</a></li>
+          `}
+        </ul>
+      </nav>
+    </div>
+  `;
+};
