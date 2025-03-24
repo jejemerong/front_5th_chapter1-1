4e295c7cdc615cd1585b3ce55a94457abe4753a9
jejemerong <9708jjw@gmail.com>
2025-03-25 05:22:10 +0900

    feat: 헤더 클릭요소 분기 처리

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 // TODO: 헤더 상태
 export const Header = ({ isLoggedIn }) => {
+  const currentPath = window.location.pathname;
+  console.log("currentPath", currentPath);
   return /* HTML */ `
     <div>
       <header class="bg-blue-600 text-white p-4 sticky top-0">
@@ -7,12 +9,33 @@ export const Header = ({ isLoggedIn }) => {
       </header>
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="/" class="text-blue-600">홈</a></li>
+          <li>
+            <a
+              href="/"
+              class="${currentPath === "/"
+                ? "text-blue-600 font-bold"
+                : "text-gray-600"}"
+              >홈</a
+            >
+          </li>
           ${isLoggedIn
-            ? `<li><a id="profile" href="/profile" class="text-gray-600">프로필</a></li>
-          <li><a id="logout" href="/login" class="text-gray-600">로그아웃</a></li>`
-            : `<li><a id="login" href="/login" class="text-gray-600">로그인</a></li>
-          `}
+            ? `<li>
+                  <a href="/profile" class="${
+                    currentPath === "/profile"
+                      ? "text-blue-600 font-bold"
+                      : "text-gray-600"
+                  }">프로필</a>
+                </li>
+                <li>
+                  <a id="logout" href="/login" class="text-gray-600">로그아웃</a>
+                </li>`
+            : `<li>
+                  <a id="login" href="/login" class="${
+                    currentPath === "/login"
+                      ? "text-blue-600 font-bold"
+                      : "text-gray-600"
+                  }">로그인</a>
+                </li>`}
         </ul>
       </nav>
     </div>
