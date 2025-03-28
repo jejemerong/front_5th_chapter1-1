0e7837641219e7da5f92d292802e9a2470d4f8bf
jejemerong <9708jjw@gmail.com>
2025-03-28 22:40:37 +0900

    feat: 해시라우터 navigate, loadContent, handleRoute 역할 분리

--- src/main.hash.js
+++ src/main.hash.js
@@ -4,72 +4,75 @@ import ProfilePage from "./pages/ProfilePage";
 import ErrorPage from "./pages/ErrorPage";
 import { getLoginStatus } from "./utils/getLoginStatus";
 
+// TODO: prefix 를 붙인다면 "index.hash.html"
 const routes = {
   "/": () => HomePage(),
   "/login": () => LoginPage(),
   "/profile": () => ProfilePage(),
-  "/404": () => ErrorPage(),
-}; // 아 그래서 다른 분들이 prefix 이랬구나
+  404: () => ErrorPage(),
+};
 
-function loadContent(content, path) {
-  const rootElement = document.getElementById("root");
+function loadContent(content) {
+  document.getElementById("root").innerHTML = content();
+}
+
+function navigate(path) {
+  window.location.hash = path; // TODO: 근데 window 빼도 되는건가?
+}
+
+function handleRoute() {
   const isLoggedIn = getLoginStatus();
+  const path = window.location.hash.slice(1) || "/";
+  const page = routes[path]; // 페이지 들고왔지
 
-  // 로그인 -> 홈까지 같이 햦ㄹ수도있음.
+  // 로그인한 사용자가 로그인 페이지 접근 시, 홈으로 이동
   if (isLoggedIn && path === "/login") {
-    window.location.hash = "/";
-    rootElement.innerHTML = HomePage();
+    navigate("/");
+    loadContent(() => HomePage());
     return;
   }
-  // 로그인 안됐을 때 프로필 접근 제한
+
+  // 로그인 안한 사용자가 프로필 접근 시, 로그인으로 이동
   if (!isLoggedIn && path === "/profile") {
-    window.location.hash = "/login"; // 네네
-    rootElement.innerHTML = LoginPage();
-    return; // 까먹지말자!!!!!
+    window.location.hash = "/login";
+    loadContent(() => LoginPage);
+    return;
   }
-  window.location.hash = path;
-  console.log("path in loadContent", path);
-  rootElement.innerHTML = content;
-  console.log("rootElement.innerHTML", rootElement.innerHTML);
-}
 
-function handleRoute(path) {
-  // const path =
-  const route = routes[path];
-
-  if (route) {
-    const content = route();
-    loadContent(content, path);
+  if (page) {
+    navigate(path);
+    loadContent(page);
   } else {
-    loadContent(ErrorPage(), "/404");
+    loadContent(() => ErrorPage());
   }
 }
 
-// TODO: navigate 를 따로 만들어서
 const handleClick = (e) => {
   if (e.target.tagName === "A") {
-    e.preventDefault(); // 네네 a 링크가 눌렀을 때 기본 이벤트들이 있는데 그 이벤트들을 방지, 아하~!
+    e.preventDefault();
+    // TODO: e.target.href 로 변경
     switch (e.target.id) {
       case "logout":
         localStorage.removeItem("user");
-        handleRoute("/login"); // TODO: 이동하려고 하는 URL, 렌더링해야 하는 컴포넌트 //네네 네네 아그러면 window 가 피룡한 때는 겱국ㄱ 그 가려는 페이지랑 현재 페이지랑 비교한ㄴ엥 왜지 저기 현재 페이지랑 비교해서 replace 할 때도 필요한거아니에요
+        navigate("/login");
         break;
 
       case "login":
-        handleRoute("/login");
+        navigate("/login");
         break;
 
       case "profile":
-        handleRoute("/profile");
+        navigate("/profile");
         break;
 
       default:
-        handleRoute("/");
+        navigate("/");
         break;
     }
   }
 };
 
+// TODO: submit, click 이랑 따로 두면 중복요청이 많아진다. => 왜? => 그래서 if 문으로 처리 해둔 것인데
 const handleSubmit = (e) => {
   // 로그인
   if (e.target.id === "login-form") {
@@ -84,7 +87,7 @@ const handleSubmit = (e) => {
       );
       username.value = "";
       password.value = "";
-      handleRoute("/"); //TODO: 여긴가
+      navigate("/");
     }
   }
 
@@ -98,25 +101,8 @@ const handleSubmit = (e) => {
   }
 };
 
-// TODO: path 변수로 두지말고
-// const path = window.location.hash.slice(1) || "/"; // 해시 지운거
+window.addEventListener("load", () => handleRoute()); // init
+window.addEventListener("hashchange", () => handleRoute()); // hashchange
 
-// init
-// handleRoute(window.location.hash.slice(1) || "/");
-handleRoute(location.hash.replace(/^#/, "") || "/");
-
-window.addEventListener("hashchange", () =>
-  handleRoute(handleRoute(location.hash.replace(/^#/, "") || "/")),
-);
-
-// 현재 상황: 테스트 코드에서 /login 으로 경로가 바뀌면서 홈화면 렌더링
-// popstate 가 뒤늦게 바뀌면서 /login 으로 경로 바뀌고 로그인 화면 렌더링
-// 임의로 테스트 코드에서 setTimeout 으로 DOM
-
-// window.addEventListener("popstate", () => {
-//   console.log("룰ㄹ루라랄라");
-//   handleRoute(window.location.hash.slice(1) || "/");
-// });
-
-document.body.addEventListener("submit", handleSubmit); //
-document.body.addEventListener("click", handleClick); //네네 네네 아하 별개의 것
+document.body.addEventListener("submit", handleSubmit);
+document.body.addEventListener("click", handleClick);
